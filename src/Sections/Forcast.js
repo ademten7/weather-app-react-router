622bfe596463977f7b4e853807155f720c22849f
ademten82 <ademten7@gmail.com>
2021-11-02 21:21:46 +0100

    aded setOut branch

--- src/Sections/Forcast.js
+++ src/Sections/Forcast.js
@@ -1,15 +1,17 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "./Forcast.scss";
 // let url =
 // `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}&units=metric`
 
-export default class Forcast extends Component {
-  state = {
-    data: [],
-  };
+const Forcast = () => {
+  useEffect(() => {
+    fetchedData();
+  }, []);
 
-  componentDidMount() {
+  const [datas, setData] = useState([]);
+
+  const fetchedData = () => {
     navigator.geolocation.getCurrentPosition((success) => {
       // console.log(success);==>we got the object
       const { latitude, longitude } = success.coords; //==>latitude, longitude these are inside the coords obj
@@ -18,31 +20,31 @@ export default class Forcast extends Component {
         `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=73e2064f98023d1456001375d4615ea5&units=metric`
       )
         .then((response) => response.json())
-        .then((data) => {
-          this.setState({ data: data.daily });
+        .then((result) => {
+          setData([result.daily]);
         });
     });
-  }
+  };
 
-  render() {
-    console.log(this.state.data);
+  console.log(datas);
 
-    return (
-      <div className="days">
-        <ul className="forcast">
-          {this.state.data.map((data, index) => {
-            return (
-              <li key={index}>
-                <NavLink
-                  className="day-list"
-                  activeClassName="myActiveClass"
-                  to={{ pathname: `/forcast/Day ${index + 1}`, state: data }}
-                >{`${index + 1}.DAY`}</NavLink>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="days">
+      <ul className="forcast">
+        {datas.map((data, index) => {
+          return (
+            <li key={index}>
+              <NavLink
+                className="day-list"
+                activeClassName="myActiveClass"
+                to={{ pathname: `/forcast/Day ${index + 1}`, state: data }}
+              >{`${index + 1}.DAY`}</NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Forcast;
